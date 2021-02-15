9712a4f4bbfb49e456b00515a7a3b8a67b35b538
ms77grz <ms77grz@gmail.com>
2021-02-15 17:54:59 +0300

    Cart Screen ...

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -11,14 +11,42 @@ function CartScreen({ match, location, history }) {
   const dispatch = useDispatch()
   const cart = useSelector((state) => state.cart)
   const { cartItems } = cart
-  console.log(cartItems)
+  // console.log(cartItems)
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty))
       console.log(productId)
     }
   }, [dispatch, productId, qty])
-  return <div></div>
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message variant='info'>
+            Your cart is empty <Link to='/'>Go Back</Link>{' '}
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}></Col>
+    </Row>
+  )
 }
 
 export default CartScreen
