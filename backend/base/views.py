64856e11fa3dce16afe7fdcfb4c76c278939666e
ms77grz <ms77grz@gmail.com>
2021-02-18 09:58:47 +0300

    JWT Customization / User Serializer - finished

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,7 +4,24 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .products import products
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        # data['username'] = self.user.username
+        # data['email'] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
@@ -20,6 +37,14 @@ def getRoutes(request):
         '/api/products/<update>/<id>/',
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    # print(user)
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
