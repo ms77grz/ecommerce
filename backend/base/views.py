b042b787ad5e9dc6d2c67105cd26115198fb4f16
ms77grz <ms77grz@gmail.com>
2021-02-18 11:33:58 +0300

    Protected Routes - finished

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .products import products
+from django.contrib.auth.models import User
 from .models import Product
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -12,8 +14,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
-        # data['username'] = self.user.username
-        # data['email'] = self.user.email
         serializer = UserSerializerWithToken(self.user).data
         for k, v in serializer.items():
             data[k] = v
@@ -40,10 +40,18 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
-    # print(user)
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 
